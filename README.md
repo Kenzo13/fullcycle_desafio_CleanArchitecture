fa8db3b50e9e33ddc402b8e5234d9c2ba5f54a30
kenzo13 <raphaelkenzo2586@gmail.com>
2023-01-16 09:27:34 -0300

    Desafio Full Cycle OrderRepository

---
+++ README.md
@@ -0,0 +1,17 @@
+# fullcycle_desafio_OrderRepository
+
+O desafio consiste em implementar o repositório Order com as funções update, find, findAll e tratamentos de erro se necessário.
+
+## Como testar
+
+Para verificar todos os testes: 
+
+```
+npm test
+```
+
+Para verificar apenas o teste em Order:
+
+```
+npm test src/infrastructure/repository/order.repository.spec.ts
+```
---
+++ src/domain/entity/order.spec.ts
@@ -0,0 +1,44 @@
+import Order from "./order";
+import OrderItem from "./order_item";
+
+describe("Order unit tests", () => {
+  it("Should throw error when id is empty", ()=> {
+    expect(() => {
+      let order = new Order("", "123", []);
+    }).toThrowError("Id is required");
+  });
+
+  it("Should throw error when customerId is empty", ()=> {
+    expect(() => {
+      let order = new Order("123", "", []);
+    }).toThrowError("CustomerId is required");
+  });
+
+  it("Should throw error when items are empty", ()=> {
+    expect(() => {
+      let order = new Order("123", "123", []);
+    }).toThrowError("Items are required");
+  });
+
+  it("Should calculate total", ()=> {
+    const item = new OrderItem("i1", "Item 1", 100, "p1", 2);
+    const item2 = new OrderItem("i2", "Item 2", 200, "p2", 3);
+    const order = new Order("o1", "c1", [item]);
+    
+    let total = order.total();
+
+    expect(total).toBe(200);
+
+    const order2 = new Order("o2", "c2", [item, item2]);
+    total = order2.total();
+
+    expect(total).toBe(800);
+  });
+
+  it("Should throw error if the item qtd is less or equal zero", ()=> {
+    expect(() => {
+      const item = new OrderItem("i1", "Item 1", 100, "p1", 0);
+      const order = new Order("o1", "c1", [item]);
+    }).toThrowError("Quantity must be greater than zero");
+  });
+});
